d08f6183e40fff6b690696930d8c2a0ed8e56184
MarBog-tech <61151776+MarBog-tech@users.noreply.github.com>
2022-09-30 09:47:35 +0300

    Initial commit

---
+++ Lab_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var singleton = (string x) => new List<string>().Add(x);
+
+            var _null = (List<string> x) => x.Count == 0;
+            
+            var snoc = (List<string> x, string y) => x.Add(y);
+
+            var lenght = (List<string> x) =>
+            {
+                var len = 0;
+                foreach (var elem in x)
+                    len++;
+                return len;
+            };
+        }
+    }
+}
